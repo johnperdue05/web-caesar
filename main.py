479c8bd223f4ae2adcf74d98dc7e1676c88068db
John Perdue <john_perdue05@yahoo.com>
2017-09-21 20:22:03 -0500

    Added form

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form method = "POST">
+      <label for="rot">Rotate by:</label>
+      <input type="text" name="rot" default="0" />
+      
+      <input type="submit" value="rotation" />
+      <input type="textarea" name="text" />
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
